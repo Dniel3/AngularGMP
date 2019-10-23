d7af8aedfda134924164f5591a1424402f03db2e
Dan <dhanielrhamirez@gmail.com>
2019-10-22 21:16:50 -0500

    implemented user interface in header component

--- gmp-video-app/src/app/core/header/header.component.ts
+++ gmp-video-app/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../course-model';
 
 @Component({
   selector: 'gmp-header',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  user: User = {
+    id: '1',
+    name: 'cosme',
+    lastName: 'fulanito'
+  };
+  
   constructor() { }
 
   ngOnInit() {
